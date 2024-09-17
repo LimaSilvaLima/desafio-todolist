c0ace217fbd3bca98f30eaabff41fd4484621f64
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2024-09-16 23:46:17 -0300

    iniciados os Entity

---
+++ src/main/java/br/com/giulianabezerra/desafio_todolist/entity/Todo.java
@@ -0,0 +1,18 @@
+package br.com.giulianabezerra.desafio_todolist.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+
+
+}
